4539a334d56bd66656e2d963ac0f73885f4770cc
Josh Quinzon <quinzonjosh261@gmail.com>
2024-03-18 01:35:18 +0800

    sidebar implementation

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Library from './Library'
-import Feed from './Feed'
-import Trending from './Trending'
-import Player from './Player'
-import Favorites from './Favorites'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Library from "./Library";
+import Feed from "./Feed";
+import Trending from "./Trending";
+import Player from "./Player";
+import Favorites from "./Favorites";
+import "./home.css";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
-
     <Router>
+      <div className="main-body">
+        <Sidebar />
         <Routes>
-            <Route path="/" element={<Library />}/>
-            <Route path="/feed" element={<Feed />}/>
-            <Route path="/trending" element={<Trending />}/>
-            <Route path="/player" element={<Player />}/>
-            <Route path="/favorites" element={<Favorites />}/>
+          <Route path="/" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/library" element={<Library />} />
         </Routes>
+      </div>
     </Router>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
